9594953fed08d2dddd1d1d31e93b80b134da2791
David Zhang <davidozhang@gmail.com>
2024-02-22 20:04:20 -0800

    Update schedule

--- server/schedule.py
+++ server/schedule.py
@@ -10,16 +10,17 @@ while True:
     load_dotenv(find_dotenv())
 
     api_key = os.getenv("API_KEY")
+    timezone = os.getenv("TIMEZONE")
+    quiet_hour_start = os.getenv("QUIET_HOUR_START")
+    quiet_hour_end = os.getenv("QUIET_HOUR_END")
+
     os.system(
         f"""~/pixlet render ~/tidbyt/pixlet/flights.star && ~/pixlet push --background --installation-id Flights --api-token "{api_key}" "agreeably-boss-glad-cougar-2e5" ~/tidbyt/pixlet/flights.webp"""
     )
 
-    pacificTimeNow = datetime.now(pytz.timezone("America/Los_Angeles"))
+    now = datetime.now(pytz.timezone(timezone))
 
-    if 0 < pacificTimeNow.hour <= 2:
-        time.sleep(60)
-    elif 2 < pacificTimeNow.hour < 8:
+    if int(quiet_hour_start) < now.hour < int(quiet_hour_end):
         time.sleep(3600)
     else:
         time.sleep(30)
-
