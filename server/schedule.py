6e193eb54745dd2d3509424dcf43de696398faf3
David Zhang <davidozhang@gmail.com>
2024-02-22 21:32:40 -0800

    Update schedule

--- server/schedule.py
+++ server/schedule.py
@@ -1,16 +1,17 @@
+import glob
 import os
 import time
 
 import pytz
 
 from datetime import datetime, time as datetime_time
-from dotenv import load_dotenv, find_dotenv
+from dotenv import find_dotenv, load_dotenv
 
 def is_current_time_between(begin_time, end_time, timezone):
     now = datetime.now(pytz.timezone(timezone)).time()
 
     if begin_time < end_time:
-        return now >= begin_time and now <= end_time
+        return begin_time <= now <= end_time
     else: # crosses midnight
         return now >= begin_time or now <= end_time
 
@@ -31,19 +32,19 @@ while True:
     quiet_hour_refresh_frequency_in_seconds = os.getenv("QUIET_HOUR_REFRESH_FREQUENCY_IN_SECONDS")
     non_quiet_hour_refresh_frequency_in_seconds = os.getenv("NON_QUIET_HOUR_REFRESH_FREQUENCY_IN_SECONDS")
 
-    push_to_client(
-        api_key=api_key,
-        device_id=device_id,
-        render_path_without_extension="~/tidbyt/client/flights",
-        installation_id='Flights'
-    )
+    apps = glob.glob("../client/*.star")
 
-    push_to_client(
-        api_key=api_key,
-        device_id=device_id,
-        render_path_without_extension="~/tidbyt/client/hello_world",
-        installation_id='HelloWorld'
-    )
+    for app in apps:
+        render_path_without_extension = app.split(".")[0]
+        app_name_components = render_path_without_extension.split("/")[-1].split("_")
+        installation_id = ''.join([s.capitalize() for s in app_name_components])
+        
+        push_to_client(
+            api_key=api_key,
+            device_id=device_id,
+            render_path_without_extension=render_path_without_extension,
+            installation_id=installation_id
+        )
 
     quiet_hour_start_hour, quiet_hour_start_minute = quiet_hour_start_time.split(":")
     quiet_hour_end_hour, quiet_hour_end_minute = quiet_hour_end_time.split(":")
