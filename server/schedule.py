dcf9b0fd3854b2693308cd20eae07e0097d37cf1
David Zhang <davidozhang@gmail.com>
2024-02-23 03:55:35 +0000

    Update server/UI/schedule

--- server/schedule.py
+++ server/schedule.py
@@ -1,11 +1,18 @@
 import os
 import time
 
-from datetime import datetime
 import pytz
 
+from datetime import datetime
+from dotenv import load_dotenv, find_dotenv
+
 while True:
-    os.system("""./pixlet render main.star && ./pixlet push --background --installation-id Flights --api-token {{API_KEY}} "agreeably-boss-glad-cougar-2e5" ./main.webp""")
+    load_dotenv(find_dotenv())
+
+    api_key = os.getenv("API_KEY")
+    os.system(
+        f"""~/pixlet render ~/tidbyt/pixlet/flights.star && ~/pixlet push --background --installation-id Flights --api-token "{api_key}" "agreeably-boss-glad-cougar-2e5" ~/tidbyt/pixlet/flights.webp"""
+    )
 
     pacificTimeNow = datetime.now(pytz.timezone("America/Los_Angeles"))
 
@@ -15,3 +22,4 @@ while True:
         time.sleep(3600)
     else:
         time.sleep(30)
+
